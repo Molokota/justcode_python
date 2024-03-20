b7f7db046bbbb53e3fdf32d1fe58ed61b94c7ae2
Alexandr Davydov <molokota2018@gmail.com>
2024-03-20 14:01:20 +0600

    Django: Домашнее задание 14.03.24

---
+++ django/task_9/post/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from django.conf import settings 
+from django.conf.urls.static import static
+
+from .API import  PostAPIList, PostAPICrud
+
+urlpatterns = [
+    path('', views.index, name='index'), 
+    path('post/', views.post),
+    path('post/<int:post_id>/', views.ful_post),
+    path('template_example/', views.template_test),
+    path('api/book/', PostAPIList.as_view()),
+    path('api/book/<int:pk>/', PostAPICrud.as_view()),
+]
+
+
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
